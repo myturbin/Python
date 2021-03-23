04960d95bc7fc88d86e5627aa39656d09a1fc26a
myturbin <77443801+myturbin@users.noreply.github.com>
2021-03-23 15:07:13 +0100

    Update task3.py

--- task3.py
+++ task3.py
@@ -6,10 +6,10 @@ from itertools import count
 with open("zp.txt") as f_obj:
     lines = f_obj.readlines()
     zp = []
-    for index, line in enumerate(lines, start=1):
-        name, surname, salary = line.split()
-        zp.append(int(salary))
-        if int(salary) < 20000:
-            print(surname)
+for index, line in enumerate(lines, start=1):
+     name, surname, salary = line.split()
+     zp.append(int(salary))
+     if int(salary) < 20000:
+         print(surname)
     print('Average salary:', sum(zp)/index)
 
